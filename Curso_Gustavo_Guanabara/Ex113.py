d6269eeaa8db57d1ffa8ed66e1936a061803f37e
Nathan Fonseca <61804011+Nathan120@users.noreply.github.com>
2020-09-30 16:27:34 -0300

    realocação

--- Curso_Gustavo_Guanabara/Ex113.py
+++ Curso_Gustavo_Guanabara/Ex113.py
@@ -1,2 +1,11 @@
 try:
-    a = int(input())
+    a = int(input('Numero:'))
+    b = int(input('Denominador:'))
+    r = a/b
+except Exception as erro:
+    print('Infelixxmente tivemos um problema :(')
+    print(f' O problema encontrado foi {erro.__class__}')
+else:
+    print(f'O resultado é {r}')
+finally:
+    print('tchau')
